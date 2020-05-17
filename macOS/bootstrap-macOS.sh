44f8ac8ad7090059aaecc1ee0197983490597fa3
Pieter Smit <pieter@pietersoftware.com>
2020-05-17 12:51:20 +0200

    Update bootstrap script

--- macOS/bootstrap-macOS.sh
+++ macOS/bootstrap-macOS.sh
@@ -5,14 +5,17 @@ echo ""
 echo "Starting the bootstrap process..."
 echo ""
 
-scripts="homebrew-basic.sh
-setDefaults.sh"
+#scripts="homebrew-basic.sh
+#setDefaults.sh"
+#
+#for script in ${scripts}
+#do
+#    echo ""
+#    source ${scripts}
+#    echo ""
+#done
 
-for script in ${scripts}
-do
-    echo ""
-    source ${scripts}
-    echo ""
-done
+# Configure iStat Menus
+cp ./init/com.bjango.istatmenus6.extras.plist ~/Library/Preferences/
 
 echo "Bootstrap completed."
